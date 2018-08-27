d7b486863c4072344f9cdd43df56bba734d4a29d
Tom King <design.tomking@gmail.com>
2018-08-27 15:26:38 -0600

    Setup router and redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { rootReducer } from './reducers';
+import App from './App';
+import './index.css';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(rootReducer, devTools)
+
+ReactDOM.render(
+<Provider store={store}>
+  <BrowserRouter >
+    <App />
+  </BrowserRouter>
+</Provider>, 
+document.getElementById('root'));
 registerServiceWorker();
