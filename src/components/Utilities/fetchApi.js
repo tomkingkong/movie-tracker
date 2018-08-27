d032d241efeac3b48371debd51ef2cbd98cee35c
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-08-27 16:41:46 -0600

    Set up initial file structure

---
+++ src/components/Utilities/fetchApi.js
@@ -0,0 +1,29 @@
+import API_KEY from '../key';
+
+export const discoverMovies = (url) => {
+  const url = `https://api.themoviedb.org/3/discover/movie/?api_key=${API_KEY}`
+
+}
+
+export const fetchUserFavorites = (user) => {
+
+}
+
+export const addUserFavorite = (user, id) =>{
+
+}
+
+export const deleteUserFavorite = (user, id) =>{
+
+}
+
+export const addNewUser = (user) => {
+
+}
+
+export const userSignIn = (user) => {
+
+}
+
+
+
