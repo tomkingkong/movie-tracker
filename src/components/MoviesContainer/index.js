fd7da020f541c94f71244981b615d6e588aee4af
Tom King <design.tomking@gmail.com>
2018-08-27 22:52:28 -0600

    Map card components in moviescontainer component

--- src/components/MoviesContainer/index.js
+++ src/components/MoviesContainer/index.js
@@ -6,9 +6,7 @@ import './Container.css';
 export const MoviesContainer = ({movies}) => {
   if (!movies) return (<section></section>)
   const displayMovieCards = movies.map((movie, i) => {
-    return (
-      <Card {...movie} key={movie.title+i} />
-    )
+    return (<Card {...movie} key={movie.title+i} />)
   })
   return (
     <section className="Container">
