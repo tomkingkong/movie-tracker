86485f616ce2a38bb439277a51b02c8ae5c2e230
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-08-27 18:02:07 -0600

    Import discoverMovies from fetchApis
    Call discoverMovies in ComponentDidMount

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import {discoverMovies} from '../../Utilities/fetchApi';
 
 class App extends Component {
+  constructor() {
+    super()
+  }
+
+  async componentDidMount() {
+    const hanksMovies = await discoverMovies();
+    console.log(hanksMovies);
+  }
+
   render() {
     return (
       <div className="App">
