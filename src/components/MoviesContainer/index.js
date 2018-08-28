e522b28d0978a9004516c93cc77620918235e1a0
Tom King <design.tomking@gmail.com>
2018-08-27 22:52:04 -0600

    Create default display for moviecontainer component

--- src/components/MoviesContainer/index.js
+++ src/components/MoviesContainer/index.js
@@ -3,7 +3,8 @@ import React from 'react';
 import Card from '../Card';
 import './Container.css';
 
-export const MoviesContainer = (movies) => {
+export const MoviesContainer = ({movies}) => {
+  if (!movies) return (<section></section>)
   const displayMovieCards = movies.map((movie, i) => {
     return (
       <Card {...movie} key={movie.title+i} />
