46e7e011d17fc9e45e2c1545e18172cf77a3aebf
Tom King <design.tomking@gmail.com>
2018-08-29 17:39:51 -0600

    Create form router

--- src/components/MoviesContainer/index.js
+++ src/components/MoviesContainer/index.js
@@ -4,7 +4,6 @@ import Card from '../Card';
 import './MoviesContainer.css';
 
 export const MoviesContainer = ({movies}) => {
-  debugger
   if (!movies) return (<section></section>)
   const displayMovieCards = movies.map((movie, i) => {
     return (<Card {...movie} key={movie.title+i} />)
