038fb8522e50f832adbe93654f27895a48b72b39
Chris Boylen <billpolster@gmail.com>
2018-09-01 16:45:09 -0600

    Add mockData

---
+++ src/Utilities/mockData.js
@@ -0,0 +1,228 @@
+export const mockMovie = {
+  title: "Forrest Gump",
+  vote_average: 8.4,
+  poster_path: "https://image.tmdb.org/t/p/w500/yE5d3BUhE8hCnkMUJOo1QDoOGNz.jpg",
+  movie_id: 13,
+  overview: "A man with a low IQ has accomplished great things in his life and been present during significant historic events - in each case, far exceeding what anyone imagined he could do. Yet, despite all the things he has attained, his one true love eludes him. 'Forrest Gump' is the story of a man who rose above his challenges, and who proved that determination, courage, and love are more important than ability.",
+  release_date: "1994-07-06"
+};
+
+export const hanksCredits = {
+    "cast": [{
+          "character": "Forrest Gump",
+          "credit_id": "52fe420ec3a36847f800074f",
+          "poster_path": "/yE5d3BUhE8hCnkMUJOo1QDoOGNz.jpg",
+          "id": 13,
+          "video": false,
+          "vote_count": 11749,
+          "adult": false,
+          "backdrop_path": "/7c9UVPPiTPltouxRVY6N9uugaVA.jpg",
+          "genre_ids": [
+            35,
+            18,
+            10749
+          ],
+          "original_language": "en",
+          "original_title": "Forrest Gump",
+          "popularity": 23.844,
+          "title": "Forrest Gump",
+          "vote_average": 8.4,
+          "overview": "A man with a low IQ has accomplished great things in his life and been present during significant historic events - in each case, far exceeding what anyone imagined he could do. Yet, despite all the things he has attained, his one true love eludes him. 'Forrest Gump' is the story of a man who rose above his challenges, and who proved that determination, courage, and love are more important than ability.",
+          "release_date": "1994-07-06"
+        },
+        {
+          "character": "Himself (voice)",
+          "credit_id": "52fe4211c3a36847f8001551",
+          "poster_path": "/eCytnEriVur3rT47NWfkgPXD9qs.jpg",
+          "id": 35,
+          "video": false,
+          "vote_count": 3638,
+          "adult": false,
+          "backdrop_path": "/gMjtdTP6HIi7CDilqXwnX8vouxO.jpg",
+          "genre_ids": [
+            16,
+            35,
+            10751
+          ],
+          "original_language": "en",
+          "original_title": "The Simpsons Movie",
+          "popularity": 12.281,
+          "title": "The Simpsons Movie",
+          "vote_average": 6.9,
+          "overview": "After Homer accidentally pollutes the town's water supply, Springfield is encased in a gigantic dome by the EPA and the Simpsons are declared fugitives.",
+          "release_date": "2007-07-27"
+        },
+        {
+          "character": "Paul Edgecomb",
+          "credit_id": "52fe424ac3a36847f8012bc7",
+          "poster_path": "/sOHqdY1RnSn6kcfAHKu28jvTebE.jpg",
+          "id": 497,
+          "video": false,
+          "vote_count": 6339,
+          "adult": false,
+          "backdrop_path": "/Rlt20sEbOQKPVjia7lUilFm49W.jpg",
+          "genre_ids": [
+            80,
+            18,
+            14
+          ],
+          "original_language": "en",
+          "original_title": "The Green Mile",
+          "popularity": 20.451,
+          "title": "The Green Mile",
+          "vote_average": 8.4,
+          "overview": "A supernatural tale set on death row in a Southern prison, where gentle giant John Coffey possesses the mysterious power to heal people's ailments. When the cell block's head guard, Paul Edgecomb, recognizes Coffey's miraculous gift, he tries desperately to help stave off the condemned man's execution.",
+          "release_date": "1999-12-10"
+        },
+        {
+          "character": "Jim Lovell",
+          "credit_id": "52fe4253c3a36847f801595d",
+          "poster_path": "/7ksHQG9qbEyPBBTAe8NfJVAipmJ.jpg",
+          "id": 568,
+          "video": false,
+          "vote_count": 2284,
+          "adult": false,
+          "backdrop_path": "/dx7htIWzPs50HEWP0lyvjfyfKyt.jpg",
+          "genre_ids": [
+            18,
+            36
+          ],
+          "original_language": "en",
+          "original_title": "Apollo 13",
+          "popularity": 14.443,
+          "title": "Apollo 13",
+          "vote_average": 7.4,
+          "overview": "The true story of technical troubles that scuttle the Apollo 13 lunar mission in 1971, risking the lives of astronaut Jim Lovell and his crew, with the failed journey turning into a thrilling saga of heroism. Drifting more than 200,000 miles from Earth, the astronauts work furiously with the ground crew to avert tragedy.",
+          "release_date": "1995-06-30"
+        },
+        {
+          "character": "Robert Langdon",
+          "credit_id": "52fe4259c3a36847f8017445",
+          "poster_path": "/5IqaT3fza0Y9vKazjGwDGsNuuS8.jpg",
+          "id": 591,
+          "video": false,
+          "vote_count": 4085,
+          "adult": false,
+          "backdrop_path": "/hcAMO4LF6G2hvsTpNgmY1kFj3jJ.jpg",
+          "genre_ids": [
+            9648,
+            53
+          ],
+          "original_language": "en",
+          "original_title": "The Da Vinci Code",
+          "popularity": 12.172,
+          "title": "The Da Vinci Code",
+          "vote_average": 6.6,
+          "overview": "When the curator of the Louvre is found murdered in the famed museum's hallowed halls, Harvard professor, Robert Langdon and cryptographer, Sophie Neve must untangle a deadly web of deceit involving the works of Leonardo da Vinci.",
+          "release_date": "2006-05-19"
+        },
+        {
+          "character": "Viktor Navorski",
+          "credit_id": "52fe4259c3a36847f801762f",
+          "poster_path": "/f4Dup6awDfDqAHKgWqNJ2HFw1qN.jpg",
+          "id": 594,
+          "video": false,
+          "vote_count": 2929,
+          "adult": false,
+          "backdrop_path": "/39wUqkmY9Ikwo6DTgSRzwAMVqkS.jpg",
+          "genre_ids": [
+            35,
+            18
+          ],
+          "original_language": "en",
+          "original_title": "The Terminal",
+          "popularity": 10.27,
+          "title": "The Terminal",
+          "vote_average": 7.2,
+          "overview": "Viktor Navorski is a man without a country; his plane took off just as a coup d'etat exploded in his homeland, leaving it in shambles, and now he's stranded at Kennedy Airport, where he's holding a passport that nobody recognizes. While quarantined in the transit lounge until authorities can figure out what to do with him, Viktor simply goes on living – and courts romance with a beautiful flight attendant.",
+          "release_date": "2004-06-17"
+        },
+        {
+          "character": "Carl Hanratty",
+          "credit_id": "52fe4263c3a36847f801a643",
+          "poster_path": "/MywWCQGJNUr5kivAQ7eseCG7rm.jpg",
+          "id": 640,
+          "video": false,
+          "vote_count": 5677,
+          "adult": false,
+          "backdrop_path": "/ei0BdAPDALPp21OEZqknG43WOmO.jpg",
+          "genre_ids": [
+            80,
+            18
+          ],
+          "original_language": "en",
+          "original_title": "Catch Me If You Can",
+          "popularity": 11.912,
+          "title": "Catch Me If You Can",
+          "vote_average": 7.8,
+          "overview": "A true story about Frank Abagnale Jr. who, before his 19th birthday, successfully conned millions of dollars worth of checks as a Pan Am pilot, doctor, and legal prosecutor. An FBI agent makes it his mission to put him behind bars. But Frank not only eludes capture, he revels in the pursuit.",
+          "release_date": "2002-12-25"
+        },
+        {
+          "character": "Captain John H. Miller",
+          "credit_id": "52fe4283c3a36847f8024aef",
+          "poster_path": "/miDoEMlYDJhOCvxlzI0wZqBs9Yt.jpg",
+          "id": 857,
+          "video": false,
+          "vote_count": 6921,
+          "adult": false,
+          "backdrop_path": "/gRtLcCQOpYUI9ThdVzi4VUP8QO3.jpg",
+          "genre_ids": [
+            18,
+            36,
+            10752
+          ],
+          "original_language": "en",
+          "original_title": "Saving Private Ryan",
+          "popularity": 17.975,
+          "title": "Saving Private Ryan",
+          "vote_average": 8,
+          "overview": "As U.S. troops storm the beaches of Normandy, three brothers lie dead on the battlefield, with a fourth trapped behind enemy lines. Ranger captain John Miller and seven men are tasked with penetrating German-held territory and bringing the boy home.",
+          "release_date": "1998-07-24"
+        },
+        {
+          "character": "Sam Baldwin",
+          "credit_id": "52fe4283c3a36847f8024bd9",
+          "poster_path": "/afkYP15OeUOD0tFEmj6VvejuOcz.jpg",
+          "id": 858,
+          "video": false,
+          "vote_count": 892,
+          "adult": false,
+          "backdrop_path": "/p3G0VLxXu9AK0WXXsGrjfXr9wrA.jpg",
+          "genre_ids": [
+            35,
+            18,
+            10749
+          ],
+          "original_language": "en",
+          "original_title": "Sleepless in Seattle",
+          "popularity": 6.344,
+          "title": "Sleepless in Seattle",
+          "vote_average": 6.6,
+          "overview": "A young boy who tries to set his dad up on a date after the death of his mother. He calls into a radio station to talk about his dad’s loneliness which soon leads the dad into meeting a Journalist Annie who flies to Seattle to write a story about the boy and his dad. Yet Annie ends up with more than just a story in this popular romantic comedy.",
+          "release_date": "1993-06-24"
+        },
+        {
+          "character": "Woody (voice)",
+          "credit_id": "52fe4284c3a36847f8024f95",
+          "poster_path": "/rhIRbceoE9lR4veEXuwCC2wARtG.jpg",
+          "id": 862,
+          "video": false,
+          "vote_count": 8130,
+          "adult": false,
+          "backdrop_path": "/dji4Fm0gCDVb9DQQMRvAI8YNnTz.jpg",
+          "genre_ids": [
+            16,
+            35,
+            10751
+          ],
+          "original_language": "en",
+          "original_title": "Toy Story",
+          "popularity": 20.968,
+          "title": "Toy Story",
+          "vote_average": 7.9,
+          "overview": "Led by Woody, Andy's toys live happily in his room until Andy's birthday brings Buzz Lightyear onto the scene. Afraid of losing his place in Andy's heart, Woody plots against Buzz. But when circumstances separate Buzz and Woody from their owner, the duo eventually learns to put aside their differences.",
+          "release_date": "1995-11-22"
+        }
+      ]
+    }
