2a254e1879d1ee28626cb5b2313e6a5532f30083
Chris Boylen <billpolster@gmail.com>
2018-09-01 17:40:18 -0600

    Add and pass test  fetchUserFavorites in fetchApi.test
    Should return an object if status code is ok

--- src/Utilities/fetchApi.test.js
+++ src/Utilities/fetchApi.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { API_KEY } from '../key';
-import { discoverMovies } from './fetchApi';
-import { hanksCredits } from './mockData';
+import { discoverMovies, fetchUserFavorites } from './fetchApi';
+import { mockMovie, hanksCredits } from './mockData';
+
 
 
 
@@ -15,30 +16,17 @@ describe('fetchApi', () => {
     beforeEach(() => {
       window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
         json: () => Promise.resolve( hanksCredits )
-      }))
-    })
+      }));
+    });
     
     it('should be invoked with correct params', async () => {
-      const expected = `https://api.themoviedb.org/3/person/31/movie_credits?api_key=${API_KEY}&language=en-US`
+      const expected = `https://api.themoviedb.org/3/person/31/movie_credits?api_key=${API_KEY}&language=en-US`;
 
-      await discoverMovies()
-
-      expect(window.fetch).toHaveBeenCalledWith(expected)
-    })
-  describe('fetchUserFavorites', () => {
-    beforeEach(() => {
-      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-        json: () => Promise.resolve(hanksCredits)
-      }))
-  })
-    it('should be invoked with correct params', async () => {
-      const expected = `http://localhost:3000/api/users/1/favorites`;
-
-      await fetchUserFavorites(1);
+      await discoverMovies();
 
       expect(window.fetch).toHaveBeenCalledWith(expected);
     });
-})
+
     it.skip('should return an object if status code ok', async () => {
       const expected = hanksCredits;
 
@@ -59,5 +47,9 @@ describe('fetchApi', () => {
 
       expect(window.fetch).toHaveBeenCalledWith(expected);
     });
+
+    it('should return an object if status code is ok', async () => {
+      await expect(fetchUserFavorites(1)).resolves.toEqual(hanksCredits);
+    })
   })
 });
