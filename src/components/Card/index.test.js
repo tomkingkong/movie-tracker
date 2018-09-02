c840fd82db10c8572dd0ef1afcf2d44111cc72f9
Tom King <design.tomking@gmail.com>
2018-09-02 00:45:44 -0600

    Add snapshot test to card component

---
+++ src/components/Card/index.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { createMockStore } from 'redux-test-utils';
+
+import { Card } from '.';
+
+describe('Card component', () => {
+  let mockHistory;
+  let testState;
+  let mockMovie;
+  let mockUser;
+  let wrapper;
+  let store;
+  let mockRemoveFavorite;
+  let mockAddFavorite;
+  let mockFn;
+  let e;
+
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      Promise.resolve({ json:() => Promise.resolve({})})
+    })
+    mockRemoveFavorite = jest.fn();
+    mockAddFavorite = jest.fn();
+    mockFn = jest.fn()
+    e = {stopPropagation:jest.fn()}
+    mockMovie = {
+      title: 'title', 
+      poster_path: 'words.jpg', 
+      vote_average: 'avg', 
+      overview: 'overview', 
+      release_date: 'release',
+      movie_id: 35
+    }
+    mockUser = { id: 1, name:'tim' }
+    mockHistory = { push: jest.fn().mockImplementation(() => {}) }
+    testState = { movies: [mockMovie], favorites: [], user: {mockUser} };
+    store = createMockStore(testState);
+  })
+
+  it('should match snapshot with props passed', () => {
+    wrapper = shallow(
+      <Card 
+        movie={mockMovie}
+        history={mockHistory} 
+        key={mockMovie.title+1}  
+      />);
+    expect(wrapper).toMatchSnapshot();
+  });
