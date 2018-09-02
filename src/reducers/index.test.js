5d059cf483e7d1e8266f5c4f79bd28b9bbd0f622
Tom King <design.tomking@gmail.com>
2018-09-02 00:51:08 -0600

    Add moviesreducer test suite

--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -1,7 +1,8 @@
 import { favoritesReducer } from './favoritesReducer';
 import { moviesReducer } from './moviesReducer';
-import { userReducer } from './userReducer';
 import { alertReducer } from './alertReducer';
+import { userReducer } from './userReducer';
+import { rootReducer } from '.';
 import * as actions from '../actions';
 
 describe('favoritesReducer', () => {
@@ -9,7 +10,7 @@ describe('favoritesReducer', () => {
     const expected = [];
     const result = favoritesReducer(undefined, {});
     expect(result).toEqual(expected);
-  })
+  });
 
   it('should return the state with a new favorite', () => {
     const initialState = [];
@@ -17,22 +18,31 @@ describe('favoritesReducer', () => {
     const expected = [movie];
     const result = favoritesReducer(initialState, actions.addFavorite(movie));
     expect(result).toEqual(expected);
-  })
+  });
 
   it('should return the state with a removed favorite', () => {
-    const currentState = [{movie_id: 5, title: 'Big'}]
+    const currentState = [{movie_id: 5, title: 'Big'}];
     const movie_id = 5;
     const expected = [];
     const result = favoritesReducer(currentState, actions.removeFavorite(movie_id));
     expect(result).toEqual(expected);
-  })
+  });
 
   it('should update favorites when a user clicks the favorites button', () => {
     const initialState = [];
-    const movies = [{title: 'Big'}, {title: 'The Green Mile'}]
+    const movies = [{title: 'Big'}, {title: 'The Green Mile'}];
     const expected = movies;
     const result = favoritesReducer(initialState, actions.updateFavorites(movies));
     expect(result).toEqual(expected);
-  })
+  });
+});
+
+describe('moviesReducer', () => {
+  it('should return the initial state', () => {
+    const expected = [];
+    const result = moviesReducer(undefined, {});
+    expect(result).toEqual(expected);
+  });
+
 
 })
