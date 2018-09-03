b509c94e7b464f9c04724c2b1bb288a925c9ea1e
Tom King <design.tomking@gmail.com>
2018-09-03 09:04:42 -0600

    Refactor fetchcurry to take indefinite number of paths

--- src/Utilities/fetchApi.js
+++ src/Utilities/fetchApi.js
@@ -1,8 +1,8 @@
 import { API_KEY } from '../key';
 import { cleanMovieData } from './Cleaner';
 
-export const fetchCurry = (originUrl) => (end3='') => (end2='') => (end1='') => (method='') => async (body={}, options) => {  
-  const url = `${originUrl}${end3}${end2}${end1}`;
+export const fetchCurry = (originUrl) => (...paths) => (method='') => async (body={}, options) => {  
+  const url = originUrl + paths.join('');
   const payload =  options || { method, body: JSON.stringify(body), headers: {'Content-Type': 'application/json'} };
   try {
     const response = await fetch(url, payload)
@@ -13,17 +13,17 @@ export const fetchCurry = (originUrl) => (end3='') => (end2='') => (end1='') => 
   }
 }
 
-const database = 'http://localhost:3000/api/users/';
+const users = 'http://localhost:3000/api/users/';
 
-export const removeFavoriteFetch = (userId, movieId) => fetchCurry(database)(userId)('/favorites/')(movieId)('DELETE')({user_id:userId, movie_id:movieId});
-export const addFavoriteFetch = (movie) => fetchCurry(database)()('favorites')('/new')('POST')(movie);
-export const fetchFavorites = (userId) => fetchCurry(database)()(userId)('/favorites')()();
-export const signUpUserFetch = (user) => fetchCurry(database)()()('new')('POST')(user);
-export const logInUserFetch = (user) => fetchCurry(database)()()()('POST')(user);
+export const removeFavoriteFetch = (user_id, movie_id) => fetchCurry(users)(user_id, '/favorites/', movie_id)('DELETE')({user_id, movie_id});
+export const addFavoriteFetch = (movie) => fetchCurry(users)('favorites', '/new')('POST')(movie);
+export const fetchFavorites = (userId) => fetchCurry(users)(userId, '/favorites')()();
+export const signUpUserFetch = (user) => fetchCurry(users)('new')('POST')(user);
+export const logInUserFetch = (user) => fetchCurry(users)()('POST')(user);
 
 export const fetchHanksMovies = async () => {
   const imdb = 'https://api.themoviedb.org/3/person/31/movie_credits?api_key=';
-  const hanksCredits = await fetchCurry(imdb)(API_KEY)('&language=en-US')()()({},{});
+  const hanksCredits = await fetchCurry(imdb)(API_KEY, '&language=en-US')()({},{});
   if (!hanksCredits) return;
   const hanksMovies = hanksCredits.cast.map(credit => (cleanMovieData(credit)));
   return hanksMovies;
