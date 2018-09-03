600559d82f63df001512ae6a4549c9ce1059efb2
Tom King <design.tomking@gmail.com>
2018-09-03 09:14:18 -0600

    Clean up login tests

--- src/containers/UserInputForm/LoginUser.test.js
+++ src/containers/UserInputForm/LoginUser.test.js
@@ -1,22 +1,59 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import LogInUser from './LogInUser';
+
+import { LogInUser, mapDispatchToProps } from './LogInUser';
+import { loginUser, updateFavorites, alertUser } from '../../actions';
+
 
 describe('LogInUser', () => {
   describe('LogInUser Component', () => {
     let wrapper;
+    let login; 
+    let history; 
+    let updateFavorites; 
+    let alertUser;
+    let e;
 
     beforeEach(() => {
-      wrapper = shallow(<LogInUser />);
+      login = jest.fn(); 
+      history = { push: jest.fn().mockImplementation(() => ({location:'/user'}))}
+      updateFavorites = jest.fn();
+      alertUser = jest.fn();
+      e = {preventDefault:jest.fn()}
+
+      window.fetch = jest.fn().mockImplementation(() => (
+        Promise.resolve({ json:() => Promise.resolve({data:{id:1}})})
+      ))
+
+      wrapper = shallow(
+        <LogInUser 
+          login={login}
+          history={history}
+          updateFavorites={updateFavorites}
+          alertUser={alertUser}
+        />)
     })
 
     it('should match snapshot', () => {
-      expect(wrapper.html()).toMatchSnapshot()
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should call a reset on alert user on link to sign up', () => {
+      wrapper.find('NavLink').simulate('click');
+      expect(alertUser).toHaveBeenCalled();
     });
 
     describe('handleChange', () => {
       it('should set state when invoked', () => {
+        const wrapper = shallow(<LogInUser />);
+        let e = {target:{value:'foo', name:'email'}}
 
+        wrapper.instance().handleChange(e);
+        expect(wrapper.state().email).toEqual('foo');
+
+        e = {target:{value:'foo', name:'password'}}
+        wrapper.instance().handleChange(e);
+        expect(wrapper.state().password).toEqual('foo');
       });
     });
 
